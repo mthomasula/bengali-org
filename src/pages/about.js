184ea0390a9115b4c5e71bfbaa9024d4c89665be
mthomasula <michaelthomasula@gmail.com>
2021-04-30 17:48:38 -0400

    Set up navigation and created pages based on mockup

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout';
 
-export default function about() {
+export default function About() {
     return (
-        <div>
-            <h1>
-                about page
-            </h1>
-        </div>
+        <Layout>
+            <div>
+                <h1>about page</h1>
+                <p>blah blah blah blah blah blah</p>
+                <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and
+            publishing industries for previewing layouts and visual mockups.</p>
+            </div>
+        </Layout>
     )
 }
---
+++ src/pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout';
+
+export default function Services() {
+    return (
+        <Layout>
+        <div>
+            <h1>services page</h1>
+        </div>
+        </Layout>
+    )
+}
