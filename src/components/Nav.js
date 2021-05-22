0d07a146de4ced10aecbb9f24844b1bf094f8d3a
Michael Thomasula <michaelthomasula@gmail.com>
2021-05-22 12:30:44 -0400

    Added a few things

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -22,18 +22,18 @@ export default function Nav() {
 
     return (
         <header>
-        <container>
-            <Navbar className={navBar ? 'navigation active' : 'navigation'} fixed="top" >
-            
-                <Navbar.Brand href="/">Bengali Community</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
-                    <Link to="/" className="nav-link">Home</Link>
-                    <Link to="/about" className="nav-link">Our Story</Link>
-                    <Link to="/services" className="nav-link">Services</Link>
-                    <Link to="/contact" className="nav-link">Contact Us</Link> 
-            </Navbar>
-        </container>
+            <container>
+                <Navbar className={navBar ? 'navigation active' : 'navigation'} fixed="top" >
+                
+                    <Navbar.Brand href="/">Bengali Community</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/about" className="nav-link">Our Story</Link>
+                        <Link to="/services" className="nav-link">Services</Link>
+                        <Link to="/contact" className="nav-link">Contact Us</Link> 
+                </Navbar>
+            </container>
         </header>
     )
 }
--- src/pages/services.js
+++ src/pages/services.js
@@ -6,51 +6,50 @@ import CardGroup from 'react-bootstrap/CardGroup'
 
 
 export default function Services() {
+
+//Test data. Replace later
+    const servicesList = [
+        {
+            type: "Commmunity gather",
+            location: "Lora Smith Rd."
+        },
+        {
+            type: "Yoga in the park",
+            location: "Grant Park"
+        },
+        {
+            type: "Charity event",
+            location: "Rec Center"
+        },
+        {
+            type: "Movie in the park",
+            location: "Some Park"
+        },
+        
+    ];
+
+    const renderedList = servicesList.map((item) => {
+        return(
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>{item.type}</Card.Title>
+                    <Card.Text>{item.location}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+        )
+    });
+
+
+
     return (
         <Layout>
             <div className='container'>
-            
                 <CardGroup>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This card has supporting text below as a natural lead-in to additional
-                        content.{' '}
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to
-                        show that equal height action.
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
+                    {renderedList}
                 </CardGroup>
             </div>
         </Layout>
